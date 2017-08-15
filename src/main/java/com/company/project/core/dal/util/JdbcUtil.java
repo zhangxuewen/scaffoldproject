3fa8b85b3e5f77ef78fdb3b4e752ee4b6c35b7af
zhangxuewen <zhangxuewen@yuewen.com>
2017-08-15 14:46:13 +0800

    更新

--- src/main/java/com/company/project/core/dal/util/JdbcUtil.java
+++ src/main/java/com/company/project/core/dal/util/JdbcUtil.java
@@ -12,7 +12,6 @@
 package com.company.project.core.dal.util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -26,46 +25,17 @@ import java.sql.Statement;
 
 public class JdbcUtil {
 
-    // 创建数据库的基本信息
-    // 创建url
-    private static String url = "jdbc:mysql://localhost:3306/day1029?useUnicode=true&characterEncoding = GB2312 ";
-
-    // 数据库的用户名和密码
-    private static String user = "root";
-
-    private static String password = "root";
-
     public static Connection conn = null;
 
     static Statement stsm = null;
 
     /**
-     * 一：注册的驱动程序 获取连接对象的方法 静态代码块（好处是只需要加载一次，且随着类的加载而加载）
-     */
-
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("获取数据库连接对象出错");
-        }
-    }
-
-    /**
      * 二：获取连接对象 该方法返回一个连接
      */
 
-    public static Connection getConnection() {
+    public static Connection getConnection(String url, String user, String password) {
 
-        // 创建连接对象
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        conn = JdbcConnectFactory.getConnection(url, user, password);
         return conn;
 
     }
@@ -97,14 +67,14 @@ public class JdbcUtil {
     }
 
     // 执行DDL语句（创建）
-    private static void DDL(final String sql) {
-
+    private static int DDL(final String sql, String url, String user, String password) {
+        int result;
         try {
 
-            stsm = (Statement) JdbcUtil.getConnection().createStatement();
+            stsm = (Statement) JdbcUtil.getConnection(url, user, password).createStatement();
 
             // 发送sql语句
-            int result = stsm.executeUpdate(sql);
+            result = stsm.executeUpdate(sql);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -112,20 +82,22 @@ public class JdbcUtil {
             // 调用工具类的方法，关闭连接
             JdbcUtil.close(conn, stsm);
         }
+
+        return result;
     }
 
     // 执行DML语句（插入）
-    private static void DML(String sql) {
-
+    private static int DML(String sql, String url, String user, String password) {
+        int result;
         try {
             // 使用工具类获取连接对象
-            conn = JdbcUtil.getConnection();
+            conn = JdbcUtil.getConnection(url, user, password);
 
             // 创建statement对象
             stsm = conn.createStatement();
 
             // 执行sql语句
-            int result = stsm.executeUpdate(sql);
+            result = stsm.executeUpdate(sql);
             System.out.println("影响了" + result + "行");
 
         } catch (Exception e) {
@@ -135,21 +107,27 @@ public class JdbcUtil {
             // 调用工具类关闭连接
             JdbcUtil.close(conn, stsm);
         }
+
+        return result;
     }
 
     // 执行DQL语句
-    private static void DQL(String sql) {
-
+    private static int DQL(String sql, String url, String user, String password) {
+        int result = 0;
         try {
 
             // 调用工具类连接对象
-            conn = JdbcUtil.getConnection();
+            conn = JdbcUtil.getConnection(url, user, password);
 
             // 创建statement对象
             stsm = conn.createStatement();
 
             // 执行sql语句,返回的是RrsultSet对象
             ResultSet rs = stsm.executeQuery(sql);
+
+            if (rs != null) {
+                result = 1;
+            }
 
             // 查看第二行数据
 
@@ -169,5 +147,6 @@ public class JdbcUtil {
             // 调用工具类关闭连接,这里要多关闭一个连接：ResultSet，工具类的关闭方法要添加它
             JdbcUtil.close(conn, stsm);
         }
+        return result;
     }
 }
